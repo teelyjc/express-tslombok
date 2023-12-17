0aa4d53ddf18cbfb868e9d7358d849b04d367a62
Tannatee Juchan <ju.tannatee2543@gmail.com>
2023-12-17 19:17:34 +0700

    feat: use Getter & Setter for setup cors

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,9 @@
 import { Server } from "@/Server";
 
 const server = new Server();
+
+server.setCorsOptions({ origin: true, credentials: true });
+server.setPort(5000);
+server.setHostname("0.0.0.0");
+
 server.start();
--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 
-import http from "http";
+import http, { Server as HTTPServer } from "http";
 
 import { Logger } from "@/utils/log/Logger";
 import { LoggerFactory } from "@/utils/log/LoggerFactory";
@@ -12,14 +12,21 @@ export class Server {
   private readonly logger: Logger = LoggerFactory.createLogger();
 
   private readonly app: express.Express;
-  private readonly server: http.Server;
 
+  @Getter
+  private readonly server: HTTPServer;
+
+  @Getter
+  @Setter
   private readonly port: number = 4000;
+
+  @Getter
+  @Setter
   private readonly hostname: string = "0.0.0.0";
 
   @Setter
   @Getter
-  private corsOptions: cors.CorsOptions = {};
+  private corsOptions: CorsOptions = {};
 
   public constructor() {
     this.logger.info("Server is starting..");
@@ -33,8 +40,8 @@ export class Server {
 
     this.server.on("listening", this.onListen.bind(this));
 
-    process.on("SIGTERM", this.stop.bind(this, this.server));
-    process.on("SIGINT", this.stop.bind(this, this.server));
+    process.on("SIGTERM", this.stop.bind(this, this.getServer()));
+    process.on("SIGINT", this.stop.bind(this, this.getServer()));
   }
 
   public start() {
@@ -56,9 +63,5 @@ export class Server {
 
   private onStop(): void {
     this.logger.info("Stopped Server!");
-  }
-
-  public getServer(): http.Server {
-    return this.server;
   }
 }
